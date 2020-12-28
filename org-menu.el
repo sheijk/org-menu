e199f6e23417b63319a0981fb5bf05bb5a4fd89a
Jan Rehders <nospam@sheijk.net>
2020-12-28 17:48:30 +0100

    Create/delete table rows/columns
    - create/delete commands for table
    - toggle formula debugging
    - made more commands transient
    - reshuffled menu a bit

--- org-menu.el
+++ org-menu.el
@@ -39,7 +39,7 @@
     ("A" "archive" org-toggle-archive-tag :transient t)
     ("/" "comment" org-toggle-comment :transient t)
     ("C-w" "cut tree" org-cut-special :transient t)
-    ("C-y" "yank tree" org-paste-special)]
+    ("C-y" "yank tree" org-paste-special :transient t)]
    ["Make new"
     ("mh" "headline" org-insert-heading)
     ("mH" "headline (after)" org-insert-heading-after-current)
@@ -135,15 +135,30 @@
     ("N" "down" org-table-move-row-down :transient t)
     ("B" "left" org-table-move-column-left :transient t)
     ("F" "right" org-table-move-column-right :transient t)]
+   ["Create/delete"
+    ("mr" "row above" org-table-insert-row :transient t)
+    ("mc" "column right" org-table-insert-column :transient t)
+    ("m-" "horiz. line" org-table-insert-hline :transient t)
+    ("dr" "delete row" org-shiftmetaup :transient t)
+    ("dc" "delete column" org-shiftmetaleft :transient t)
+    ("mt" "table from region" org-table-create-or-convert-from-region)]
    ["Formulas"
-    ("e" "edit" org-table-edit-formulas)
-    ("=" "field" (lambda () (interactive) (org-table-eval-formula '(4))))
+    ("e" "edit all" org-table-edit-formulas :transient t)
+    ("=" "field" (lambda () (interactive) (org-table-eval-formula '(4))) :transient t)
     ("+" "in place" (lambda () (interactive) (org-table-eval-formula '(16))))
-    ("c" "column" org-table-eval-formula)
-    ("h" "coordinates" org-table-toggle-coordinate-overlays :transient t)]
-   ["Evaluate"
-    ("x" "evaluate" shk-org-menu-eval)]
-   [("q" "quit" transient-quit-all)]])
+    ("c" "column" org-table-eval-formula :transient t)
+    ("h" "coordinates" org-table-toggle-coordinate-overlays :transient t)
+    ("D" "debug" org-table-toggle-coordinate-overlays :transient t)]
+   ["Field"
+    ("E" "edit" org-table-edit-field :transient t)
+    ("SPC" "blank" org-table-blank-field :transient t)
+    ("RET" "from above" org-table-copy-down :transient t)]
+   ["More"
+    ("x" "evaluate" shk-org-menu-eval)
+    ("S" "shrink column" org-table-toggle-column-width :transient t)
+    ("q" "quit" transient-quit-all)]])
+
+;; shk-org-menu-table does not have a headlines Navigation menu (too crowded)
 
 (transient-define-prefix shk-org-menu ()
   "A discoverable menu to edit and view org-mode documents"
