3d05dd8941392a610e01e8c12fcdcd66f1ea0a3d
Jan Rehders <nospam@sheijk.net>
2021-01-08 23:40:37 +0100

    Rename top-level functions and menus for release

--- org-menu.el
+++ org-menu.el
@@ -1,7 +1,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; transient interface for org
 
-(defun shk-org-menu-heading-navigate-items (check-for-heading)
+(defun org-menu-heading-navigate-items (check-for-heading)
   "Items to navigate headings"
   `(["Navigate"
      ,@(when check-for-heading '(:if org-at-heading-p))
@@ -13,13 +13,13 @@
      ("M-n" "next (same level)" shk-org-next-heading :transient t)
      ("'" "by name" imenu :transient t)]))
 
-(transient-define-prefix shk-org-menu-visibility ()
+(transient-define-prefix org-menu-visibility ()
   "A menu to control visibility of org-mode items"
   ["dummy"])
 
-(transient-insert-suffix 'shk-org-menu-visibility (list 0)
+(transient-insert-suffix 'org-menu-visibility (list 0)
   `["Visibility"
-    ,@(shk-org-menu-heading-navigate-items nil)
+    ,@(org-menu-heading-navigate-items nil)
     ["Visibility"
      ("a" "all" org-show-subtree :if-not org-at-block-p :transient t)
      ("a" "all" org-hide-block-toggle :if org-at-block-p :transient t)
@@ -35,7 +35,7 @@
      ("gd" "default" (lambda () (interactive) (org-set-startup-visibility)))]
     [("q" "quit" transient-quit-all)]])
 
-(defun shk-org-menu-eval-src-items ()
+(defun org-menu-eval-src-items ()
   "Return the items to evaluate a source block"
   (list
    ["Source"
@@ -45,11 +45,11 @@
     ("k" "clear results" org-babel-remove-result-one-or-many)
     ("'" "edit" org-edit-special)]))
 
-(transient-define-prefix shk-org-menu-eval ()
+(transient-define-prefix org-menu-eval ()
   "A menu to evaluate buffers, tables, etc. in org-mode"
   ["dummy"])
 
-(transient-insert-suffix 'shk-org-menu-eval (list 0)
+(transient-insert-suffix 'org-menu-eval (list 0)
   `["Evaluation"
     ["Table"
      :if org-at-table-p
@@ -57,35 +57,35 @@
      ("1" "one iteration" (lambda () (interactive) (org-table-recalculate t)))
      ("l" "line" (lambda () (interactive) (org-table-recalculate nil)))
      ("f" "format" org-table-align :if org-at-table-p)]
-    ,@(shk-org-menu-eval-src-items)
+    ,@(org-menu-eval-src-items)
     ["Heading"
      :if-not org-in-src-block-p
      ("c" "update checkbox count" org-update-checkbox-count)]
     [("q" "quit" transient-quit-all)]])
 
-(defun shk-org-menu-insert-block (str)
+(defun org-menu-insert-block (str)
   "Insert an org mode block of type `str'"
   (interactive)
   (insert (format "#+begin_%s\n#+end_%s\n" str str)))
 
-(defun shk-org-menu-expand-snippet (snippet)
+(defun org-menu-expand-snippet (snippet)
   "Will expand the given snippet."
   (interactive)
   (insert snippet)
   (yas-expand))
 
-(transient-define-prefix shk-org-menu-insert-blocks ()
+(transient-define-prefix org-menu-insert-blocks ()
   "A menu to insert new blocks in org-mode"
   [["Insert block"
-    ("s" "source" (lambda () (interactive) (shk-org-menu-insert-block "src")))
-    ("e" "example" (lambda () (interactive) (shk-org-menu-insert-block "example")))
-    ("v" "verbatim" (lambda () (interactive) (shk-org-menu-insert-block "verbatim")))
-    ("a" "ascii" (lambda () (interactive) (shk-org-menu-insert-block "ascii")))
-    ("q" "quote" (lambda () (interactive) (shk-org-menu-insert-block "quote")))
+    ("s" "source" (lambda () (interactive) (org-menu-insert-block "src")))
+    ("e" "example" (lambda () (interactive) (org-menu-insert-block "example")))
+    ("v" "verbatim" (lambda () (interactive) (org-menu-insert-block "verbatim")))
+    ("a" "ascii" (lambda () (interactive) (org-menu-insert-block "ascii")))
+    ("q" "quote" (lambda () (interactive) (org-menu-insert-block "quote")))
     ("d" "dynamic block" org-insert-dblock)]
    [("q" "quit" transient-quit-all)]])
 
-(transient-define-prefix shk-org-menu-insert-heading ()
+(transient-define-prefix org-menu-insert-heading ()
   "A menu to insert new headings in org-mode"
   [["Heading"
     ("h" "heading" org-insert-heading)
@@ -95,15 +95,15 @@
     ("d" "drawer" org-insert-drawer)]
    [("q" "quit" transient-quit-all)]])
 
-(transient-define-prefix shk-org-menu-insert-template ()
+(transient-define-prefix org-menu-insert-template ()
   "A menu to insert new templates in org-mode"
   [["Templates"
     ("S" "structure template" org-insert-structure-template)
-    ("B" "yas blocks" (lambda () (interactive) (shk-org-menu-expand-snippet "beg")))
-    ("O" "yas options" (lambda () (interactive) (shk-org-menu-expand-snippet "opt")))]
+    ("B" "yas blocks" (lambda () (interactive) (org-menu-expand-snippet "beg")))
+    ("O" "yas options" (lambda () (interactive) (org-menu-expand-snippet "opt")))]
    [("q" "quit" transient-quit-all)]])
 
-(transient-define-prefix shk-org-menu-insert-timestamp ()
+(transient-define-prefix org-menu-insert-timestamp ()
   "A menu to insert timestamps in org-mode"
   [["Timestamp"
     ("." "active" org-time-stamp)
@@ -116,7 +116,7 @@
     ("T" "inactive" (lambda () (interactive) (org-insert-time-stamp (current-time) nil t)))]
    [("q" "quit" transient-quit-all)]])
 
-(transient-define-prefix shk-org-menu-insert-table ()
+(transient-define-prefix org-menu-insert-table ()
   "A menu to insert table items in org-mode"
   [["Table"
     ("t" "table" org-table-create-or-convert-from-region :if-not org-at-table-p)]
@@ -127,23 +127,23 @@
     ("-" "horiz. line" org-table-insert-hline :transient t)]
    [("q" "quit" transient-quit-all)]])
 
-(transient-define-prefix shk-org-menu-insert ()
+(transient-define-prefix org-menu-insert ()
   "A menu to insert new items in org-mode"
   [["Insert"
-    ("." "time" shk-org-menu-insert-timestamp)
-    ("t" "table" shk-org-menu-insert-table)
-    ("h" "heading" shk-org-menu-insert-heading)
-    ("b" "block" shk-org-menu-insert-blocks)
-    ("T" "templates" shk-org-menu-insert-template)
+    ("." "time" org-menu-insert-timestamp)
+    ("t" "table" org-menu-insert-table)
+    ("h" "heading" org-menu-insert-heading)
+    ("b" "block" org-menu-insert-blocks)
+    ("T" "templates" org-menu-insert-template)
     ("l" "link" org-insert-link)]
    [("q" "quit" transient-quit-all)]])
 
-(defun shk-org-menu-comment-line ()
+(defun org-menu-comment-line ()
   "Toggles line comment w/o moving cursor"
   (interactive)
   (save-excursion (comment-line 1)))
 
-(defun shk-org-menu-insert-text (left right)
+(defun org-menu-insert-text (left right)
   "Will insert left|right and put the curser at |"
   (if (region-active-p)
       (progn
@@ -154,7 +154,7 @@
     (insert right)
     (backward-char (length right))))
 
-(transient-define-prefix shk-org-menu-goto ()
+(transient-define-prefix org-menu-goto ()
   "Menu to go to different places by name"
   ["Go to"
    ("s" "source block" org-babel-goto-named-src-block)
@@ -162,36 +162,36 @@
    ("h" "heading" imenu)]
   [("q" "quit" transient-quit-all)])
 
-(defun shk-org-menu-at-text-p ()
+(defun org-menu-at-text-p ()
   "Returns whether point is at text"
   (not (or (org-at-heading-p)
            (org-at-table-p)
            (org-in-item-p)
            (org-in-src-block-p))))
 
-(defun shk-org-menu-text-format-items (check-for-table)
+(defun org-menu-text-format-items (check-for-table)
   (list
    `["Formatting"
-     ,@(when check-for-table '(:if shk-org-menu-at-text-p))
-     ("*" "Bold" (lambda nil (interactive) (shk-org-menu-insert-text "*" "*")))
-     ("/" "italic" (lambda nil (interactive) (shk-org-menu-insert-text "/" "/")))
-     ("_" "underline" (lambda nil (interactive) (shk-org-menu-insert-text "_" "_")))
-     ("+" "strikethrough" (lambda nil (interactive) (shk-org-menu-insert-text "+" "+")))]
+     ,@(when check-for-table '(:if org-menu-at-text-p))
+     ("*" "Bold" (lambda nil (interactive) (org-menu-insert-text "*" "*")))
+     ("/" "italic" (lambda nil (interactive) (org-menu-insert-text "/" "/")))
+     ("_" "underline" (lambda nil (interactive) (org-menu-insert-text "_" "_")))
+     ("+" "strikethrough" (lambda nil (interactive) (org-menu-insert-text "+" "+")))]
    `["Source"
-     ,@(when check-for-table '(:if shk-org-menu-at-text-p))
-     ("~" "code" (lambda nil (interactive) (shk-org-menu-insert-text "~" "~")))
-     ("=" "verbatim" (lambda nil (interactive) (shk-org-menu-insert-text "=" "=")))]))
+     ,@(when check-for-table '(:if org-menu-at-text-p))
+     ("~" "code" (lambda nil (interactive) (org-menu-insert-text "~" "~")))
+     ("=" "verbatim" (lambda nil (interactive) (org-menu-insert-text "=" "=")))]))
 
-(transient-define-prefix shk-org-menu-text-in-list ()
+(transient-define-prefix org-menu-text-in-list ()
   "Add formatting for text in lists"
   ["dummy"])
 
-(transient-insert-suffix 'shk-org-menu-text-in-list (list 0)
-  ;; These sub menus have a similar version in shk-org-menu, keep in sync
-  `[,@(shk-org-menu-text-format-items nil)
+(transient-insert-suffix 'org-menu-text-in-list (list 0)
+  ;; These sub menus have a similar version in org-menu, keep in sync
+  `[,@(org-menu-text-format-items nil)
     [("q" "quit" transient-quit-all)]])
 
-(transient-define-prefix shk-org-menu-options ()
+(transient-define-prefix org-menu-options ()
   "A menu to toggle options"
   ["Display"
    ("l" "show links" org-toggle-link-display)
@@ -199,7 +199,7 @@
    ("p" "pretty entities" org-toggle-pretty-entities)
    ("t" "timestamp overlay" org-toggle-time-stamp-overlays)])
 
-(defun shk-org-menu-in-link ()
+(defun org-menu-in-link ()
   "Returns whether we are inside a link.
 
 Conditions have been adapted from `org-insert-link'"
@@ -208,7 +208,7 @@ Conditions have been adapted from `org-insert-link'"
    (or (org-in-regexp org-link-angle-re)
        (org-in-regexp org-link-plain-re))))
 
-(defun shk-org-menu-toggle-has-checkbox ()
+(defun org-menu-toggle-has-checkbox ()
   "Toggle whether the current list item has a checkbox"
   (interactive)
   (save-excursion
@@ -218,14 +218,14 @@ Conditions have been adapted from `org-insert-link'"
       (end-of-line 1)
       (org-ctrl-c-ctrl-c '(4)))))
 
-(transient-define-prefix shk-org-menu ()
+(transient-define-prefix org-menu ()
   "A discoverable menu to edit and view org-mode documents"
   ["dummy"])
 
-(transient-insert-suffix 'shk-org-menu (list 0)
+(transient-insert-suffix 'org-menu (list 0)
   `["Org mode"
     ;; Items for headings
-    ,@(shk-org-menu-heading-navigate-items t)
+    ,@(org-menu-heading-navigate-items t)
 
     ["Move heading"
      :if org-at-heading-p
@@ -304,34 +304,34 @@ Conditions have been adapted from `org-insert-link'"
      :if org-in-item-p
      ("R" "repair" org-list-repair)
      ("*" "turn into tree" org-list-make-subtree)
-     ("t" "text formatting" shk-org-menu-text-in-list)]
+     ("t" "text formatting" org-menu-text-in-list)]
     ["Toggle"
      :if org-in-item-p
      ("-" "list item" org-toggle-item :if-not org-at-table-p :transient t)
      ("+" "list style" org-cycle-list-bullet :if-not org-at-table-p :transient t)
      ("d" "done" org-toggle-checkbox :transient t)
-     ("m" "checkbox" shk-org-menu-toggle-has-checkbox :transient t)]
+     ("m" "checkbox" org-menu-toggle-has-checkbox :transient t)]
 
     ;; Items for text
     ["Line"
-     :if shk-org-menu-at-text-p
+     :if org-menu-at-text-p
      (":" "fixed width" org-toggle-fixed-width :transient t)
-     (";" "comment" shk-org-menu-comment-line :transient t)]
-    ,@(shk-org-menu-text-format-items t)
+     (";" "comment" org-menu-comment-line :transient t)]
+    ,@(org-menu-text-format-items t)
 
     ;; Items for source blocks
-    ,@(shk-org-menu-eval-src-items)
+    ,@(org-menu-eval-src-items)
 
     ["Link"
-     :if shk-org-menu-in-link
+     :if org-menu-in-link
      ("e" "edit" org-insert-link)]
 
     ["Tasks"
-     ("v" "visibility" shk-org-menu-visibility)
-     ("x" "evaluation" shk-org-menu-eval)
-     ("i" "insert" shk-org-menu-insert-table :if org-at-table-p)
-     ("i" "insert" shk-org-menu-insert :if-not org-at-table-p)
-     ("g" "go to" shk-org-menu-goto)
-     ("o" "options" shk-org-menu-options)
+     ("v" "visibility" org-menu-visibility)
+     ("x" "evaluation" org-menu-eval)
+     ("i" "insert" org-menu-insert-table :if org-at-table-p)
+     ("i" "insert" org-menu-insert :if-not org-at-table-p)
+     ("g" "go to" org-menu-goto)
+     ("o" "options" org-menu-options)
      ""
      ("q" "quit" transient-quit-all)]])
