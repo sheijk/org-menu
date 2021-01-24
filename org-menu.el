52ef0729ea5686ab5ce4e18c157d12bb93cb3398
Jan Rehders <nospam@sheijk.net>
2021-01-24 22:47:13 +0100

    Open agenda from search menu

--- org-menu.el
+++ org-menu.el
@@ -313,14 +313,17 @@ Conditions have been adapted from `org-insert-link'"
 
 (transient-define-prefix org-menu-search-and-filter ()
   "A menu to search and filter org-mode documents"
-  ["Filter"
-   ("/" "only matching" org-sparse-tree)
-   ("q" "tags" org-tags-sparse-tree)
-   ("t" "todos" org-show-todo-tree)
-   ("d" "deadlines" org-check-deadlines)
-   ("b" "before date" org-check-before-date)
-   ("a" "after date" org-check-after-date)
-   ("D" "dates range" org-check-dates-range)])
+  ["Search and filter"
+   ["Filter"
+    ("/" "only matching" org-sparse-tree)
+    ("q" "tags" org-tags-sparse-tree)
+    ("t" "todos" org-show-todo-tree)
+    ("d" "deadlines" org-check-deadlines)
+    ("b" "before date" org-check-before-date)
+    ("a" "after date" org-check-after-date)
+    ("D" "dates range" org-check-dates-range)]
+   ["Agenda"
+    ("A" "open" org-agenda)]])
 
 ;;;###autoload
 (transient-define-prefix org-menu ()
