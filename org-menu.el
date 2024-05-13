fec91653fe0930c209714edc179e3e3b7907557d
Jan Rehders <nospam@sheijk.net>
2024-05-14 00:21:27 +0200

    Correctly create empty line
    This fixes an error making the menu unusable with recent transient versions (at
    least 20240509 but probably earlier versions, too)
    
    Fixes #9

--- org-menu.el
+++ org-menu.el
@@ -1011,7 +1011,7 @@ Code copied from lambda in org-colview.el after
          (list '("C-c C-c" "confirm capture" org-capture-finalize :if-non-nil org-capture-mode)))
      ,@(when (fboundp #'org-capture-kill)
          (list '("C-c C-k" "abort capture" org-capture-kill :if-non-nil org-capture-mode)))
-     ("" "" transient-noop)
+     ""
      ("q" "quit" transient-quit-all :if-non-nil org-menu-use-q-for-quit)]])
 
 (provide 'org-menu)
